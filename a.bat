daaf7852572b33af00cb2adb26f253795bce0fa3
shengdexiang <deyoung_shen@msn.cn>
2015-04-14 15:18:51 +0800

    do again

---
+++ a.bat
@@ -0,0 +1,3 @@
+@echo off
+for /l %%i in (1, 1, 9) do mkdir chapter_0%%i
+for /l %%i in (10, 1, 18) do mkdir chapter_%%i
---
+++ chapter_01/1.8.1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+	printf("Hello world!\n");
+
+	getchar();  /* pause until hit enter key */
+
+	return EXIT_SUCCESS;
+}
