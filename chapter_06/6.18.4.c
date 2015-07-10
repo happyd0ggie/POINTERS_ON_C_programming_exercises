f7b09472000ee4e302e2a9f10b03a0ed490d18dd
dexiang sheng <deyoung_shen@msn.cn>
2015-07-10 09:32:20 +0800

    modify source code style

--- chapter_06/6.18.4.c
+++ chapter_06/6.18.4.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TRUE 1
-#define FALSE 0
+#define TRUE 0
+#define FALSE 1
 
 char * primeNumbersBySieveOfEratosthenes(size_t n);
 
@@ -12,29 +12,37 @@ main(int argc, char *argv[])
 {
 	int i;
 	char *num = primeNumbersBySieveOfEratosthenes(10000);
-	for (i = 2; i <= 10000; ++i) {
-		if (num[i] == TRUE) {
+	for (i = 2; i <= 10000; ++i)
+	{
+		if (num[i] == TRUE)
+		{
 			printf("%7d ", i);
 		}
 	}
 
+	getchar();
 	return EXIT_SUCCESS;
 }
 
-char * primeNumbersBySieveOfEratosthenes(size_t n)
+char * 
+primeNumbersBySieveOfEratosthenes(size_t n)
 {
-	// 初始化素数数组
+	/* 初始化素数数组 */
 	char* num = (char*)malloc(sizeof(char) * n);
-	for (size_t i = 2; i < n; ++i) {
+	for (size_t i = 2; i < n; ++i)
+	{
 		num[i] = TRUE;
 	}
-	// 按照埃拉托斯特尼筛法，将为基数的倍数的所有数标记为非素数。
+	/* 按照埃拉托斯特尼筛法，将为基数的倍数的所有数标记为非素数 */
 	size_t i = 2;
-	while (i * i <= n) {
-		for (size_t c = 2, idx = 2 * i; idx < n; ++c, idx = i * c) {
+	while (i * i <= n)
+	{
+		for (size_t c = 2, idx = 2 * i; idx < n ; ++c, idx = i * c)
+		{
 			num[idx] = FALSE;
 		}
-		do {
+		do
+		{
 			++i;
 		} while (i * i <= n && num[i] == FALSE);
 	}
